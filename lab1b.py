1b942c0101d372a8c6cc9ee5521f3069cb0bd18c
Colin-Spagnuolo <cspagnuolo1@myseneca.ca>
2026-09-18 16:47:58 +0000

    Completed Lab1a in class

--- lab1b.py
+++ lab1b.py
@@ -5,20 +5,10 @@
 # Date: September 18th, 2026
 # Purpose: Use arithmetic in python.
 # Usage: python3 lab1b.py
-
-# TO-DO 1:
-#	Create a variable called "num1", take its value from user.
 num1=input("Enter a Number: ")
-#	Create another variable called "num2" and take its value from user. 
 num2=input("Enter a Number: ")
-
-# Convert the values to integers using int() function
 num1 =int(num1)
 num2=int(num2)
-
-
-# TO-DO 2:
-# Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
 print("'''Python")
 print("num1 + num2 = ", num1+num2)
 print("num1 - num2 = ", num1 - num2)
